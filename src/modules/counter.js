9f50d37b020cd8ea13e123d5f9c6aa26b01076d5
denzel <seokgyul@lgcns.com>
2023-01-17 17:37:27 +0900

    before destruction

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,26 +1,38 @@
+import { createAction, handleActions } from 'redux-actions';
+
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+// export const increase = () => ({ type: INCREASE });
+// export const decrease = () => ({ type: DECREASE });
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE); // 간단하게 action 생성 함수 선언
 
 const initialState = {
   number: 0,
 };
 
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      };
-    default:
-      return state;
-  }
-}
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState,
+);
+
+// function counter(state = initialState, action) {
+//   switch (action.type) {
+//     case INCREASE:
+//       return {
+//         number: state.number + 1,
+//       };
+//     case DECREASE:
+//       return {
+//         number: state.number - 1,
+//       };
+//     default:
+//       return state;
+//   }
+// }
 
 export default counter;
--- src/modules/todos.js
+++ src/modules/todos.js
@@ -1,30 +1,40 @@
+import { createAction, handleAction, handleActions } from 'redux-actions';
+
 const CHANGE_INPUT = 'todos/CHANGE_INPUT';
 const INSERT = 'todos/INSERT';
 const TOGGLE = 'todos/TOGGLE';
 const REMOVE = 'todos/REMOVE';
 
-export const changeInput = (input) => ({
-  type: CHANGE_INPUT,
-  input,
-});
+// export const changeInput = (input) => ({
+//   type: CHANGE_INPUT,
+//   input,
+// });
+export const changeInput = createAction(CHANGE_INPUT, (input) => input);
 let id = 3; // insert가 호출될 때마다 1씩 더해짐.
-export const insert = (text) => ({
-  type: INSERT,
-  todo: {
-    id: id++,
-    text,
-    done: false,
-  },
-});
+// export const insert = (text) => ({
+//   type: INSERT,
+//   todo: {
+//     id: id++,
+//     text,
+//     done: false,
+//   },
+// });
+export const insert = createAction(INSERT, (text) => ({
+  id: id++,
+  text,
+  done: false,
+}));
 
-export const toggle = (id) => ({
-  type: TOGGLE,
-  id,
-});
-export const remove = (id) => ({
-  type: REMOVE,
-  id,
-});
+// export const toggle = (id) => ({
+//   type: TOGGLE,
+//   id,
+// });
+// export const remove = (id) => ({
+//   type: REMOVE,
+//   id,
+// });
+export const toggle = createAction(TOGGLE, (id) => id);
+export const remove = createAction(REMOVE, (id) => id);
 
 const initialState = {
   input: '',
@@ -41,7 +51,32 @@ const initialState = {
     },
   ],
 };
-
+const todos = handleActions(
+  {
+    [CHANGE_INPUT]: (state, action) => ({ ...state, input: action.payload }),
+    [INSERT]: (state, action) => ({
+      ...state,
+      todos: state.todos.concat(action.payload),
+    }),
+    [TOGGLE]: (state, action) => ({
+      ...state,
+      todos: state.todos.map((todo) =>
+        todo.id === action.payload ? { ...todo, done: !todo.done } : todo,
+      ),
+    }),
+    [REMOVE]: (state, action) => ({
+      ...state,
+      todos: state.todos.filter((todo) => todo.id !== action.payload),
+    }),
+  },
+  initialState,
+);
+/**
+ * 
+ * @param {*} state 
+ * @param {*} action 
+ * @returns 
+ 
 function todos(state = initialState, action) {
   switch (action.type) {
     case CHANGE_INPUT:
@@ -70,5 +105,5 @@ function todos(state = initialState, action) {
       return state;
   }
 }
-
+*/
 export default todos;
